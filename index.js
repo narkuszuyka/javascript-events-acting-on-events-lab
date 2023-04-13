a8e612a26963e8d56028b85d35bd57e34299e0d4
Sukran <sukran.akyuz.93@gmail.com>
2023-04-13 13:51:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+
+let dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#adf7b6";
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 3}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    dodger.style.left = `${left + 3}px`
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
